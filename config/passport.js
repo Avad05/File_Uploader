2432092f58554fe77480bda9109053d707b23626
Avadhut <avadhutkaskar@gmail.com>
2026-02-23 15:07:53 +0530

    Prisma and express setup done

---
+++ config/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const LocalStratergy = require('passport-local');
+const bcrypt = require('bcrypt');
+import { prisma } from '../lib/prisma.js';
+
+passport.use(
+  new LocalStratergy({usernameField: 'email'}, async (email, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({where: { email } })     
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await prisma.findUnique({where: { id }});
+   // const user = rows[0];
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
---
+++ prisma/migrations/20260223070544_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Uploads" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "Uploads_pkey" PRIMARY KEY ("id")
+);
